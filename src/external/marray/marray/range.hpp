4fcad5245ab8ff82178695b2d3f1232fd2fa6491
Devin Matthews <damatthews@smu.edu>
2024-11-08 13:49:44 -0600

    Update MArray.

--- src/external/marray/marray/range.hpp
+++ src/external/marray/marray/range.hpp
@@ -21,22 +21,24 @@ namespace MArray
 namespace detail
 {
 
-template <typename T, typename=void>
-struct underlying_type_if
+template <typename T, bool=std::is_enum_v<T>>
+struct underlying_type_if;
+
+template <typename T>
+struct underlying_type_if<T, false>
 {
     typedef std::make_signed_t<T> type;
 };
 
 template <typename T>
-struct underlying_type_if<T, std::enable_if_t<std::is_enum<T>::value>>
+struct underlying_type_if<T, true>
 {
     typedef std::make_signed_t<std::underlying_type_t<T>> type;
 };
 
 template <typename... Ts> struct are_numeric;
 
-template <> struct are_numeric<>
-: std::integral_constant<bool,true> {};
+template <> struct are_numeric<> : std::true_type {};
 
 template <typename T, typename... Ts> struct are_numeric<T, Ts...>
 : std::integral_constant<bool, (std::is_integral<T>::value ||
@@ -56,9 +58,11 @@ class range_t
     template <typename U> friend class range_t;
 
     protected:
+        // NB: ordering the fields from_, to_, delta_ triggers a strange bug on
+        // clang 16 (https://godbolt.org/#z:OYLghAFBqd5QCxAYwPYBMCmBRdBLAF1QCcAaPECAMzwBtMA7AQwFtMQByARg9KtQYEAysib0QXACx8BBAKoBnTAAUAHpwAMvAFYTStJg1DIApACYAQuYukl9ZATwDKjdAGFUtAK4sGEgBykrgAyeAyYAHI%2BAEaYxCAAzKQADqgKhE4MHt6%2BASlpGQKh4VEssfFJdpgOmUIETMQE2T5%2BXIFVNQJ1DQTFkTFxibb1jc25bcM9faXliQCUtqhexMjsHOYJYcjeWADUJgluBACeyZgA%2BgTETIQKB9gmGgCCG1s7mPuHhHFMRMT3jxeZk2DG2Xj2BzcADdqn8Ac9AQRMCxkgYkZ8jqdGKwPgAVeFPbZMBQKXbXIwXAiAkwAdisz12jN2yWIqCRDkw6BAgKZvN2uN2VFZLAO9KefKZAqI51FPIl/N2WFo9RlCTFcolAHpNfyEHhSQB3EgAa1JxN2mFUZw5XI1fO1CqVKtlDPlDoFQtQIrVdt57t20pdL1djOSXmitDwyG5Ib5RJJu2%2B1zhsd5tLF8r5LLZsM5MfFmflAqhYlVGcLEoFTqYZepqflYYjUfzFYlXnSRkTSOTJHOoiRwBIx0%2BABFdgoCFyQOT0F7zkxkKsSeck79e/VgEHW3z22FgLsS95KVjR/yt9umbvO/gqFQ4oxVpcTwcxxOp8krjeqJdzxfdlf91SMJu1PXEACpfwvACyUwO9iAfD4XzPMwADYg19Vs0AYCdLRZLsfj%2BCA5l2EAD1LCANAWRVMGVGsIC4Yj01pEc6wLP8sJwq1iHwntiAgYsxFIR0aPqYjSMPc4IEPKjq0k6tGLpZjWL/RlolQTxdlQM5eJfF8IA4ggeLXf5UM0ggEDiYiDIwismPrP94IIZYGDI2hzlHJC2Qs4gADoJP2VDzBQ6jaPc3SEjHLy4h82TIO3JSEXsws1I0rSCJIMAwD0gyjLhUyouIKyBAnGzCzstiVMZRznNc9zMs88zov82k3BakKVV2eqIrM7yYpEms4tbBLgwq7dDy8Y8zk07TjLAojdmspLM3KyreWq4gXIkwbbJpFjEtG1tVzy4K0p0yxrCI0rlsUpbW2a87utin1bordaXLA8z9W2srdtKpTnpGzMDNw7jyWASl%2BIDVAxPauiGKE6UICIBSrF%2B/bMyOkhdliYAwnmxaDsZFbWze3KSAooTZLmb7htKzHuNcfHiqpJLidezAnI2sm%2BOlSn%2BvOamAeuvaXl%2BgHEWRVFfkQw4TjOZg2DPBIHmeJgvCIMlDHByGGGphEboq0mwcpSF8WViBdfQtGRqhVA8HQQV1IgYDdkt/Xy0Zd09VJBQECWWgHe0dtDPghQvGVRMXKYcc8BRegArMRyAEdzDMO01Y11RT2Ni3BY9x3uIgDPUETEjdlUYipLt9A5g0K2RY4BZaE4ABWXg/G4XhUE4VrzsscclhWRDgR4UgCE0RuFmNRIW58hIUMkFCaUkDQ2jaFuEmkZuOEkduJ9IbuOF4BQQA0MeJ4WOBYCQNAUToOJyEoW/knv%2BIiSMLgUI0M%2BaGVOIT4gNEfe0QwgNGOJwUeIDmDEGOAAeWiNoWEEDeC3zYIIWBDBaDgI4FoUgWBoheGAG4MQtAT6dzwciLW4hyH4HgjUGEZDcGWmqOrNYo9gI0WQfoPA0RrgwI8FgfeVxY5cJhMQNSSgRyUKMJGIwF8%2BAGGAAoAAangTABpYHyy4fwQQIgxDsCkDIQQigVDqBwToPQBg5GmD7jYSM0QT6QAWFpRwxVOAAFo3wvhsVYSwXAaS7HcbAhIXcxHEHtpgRxRFbA0VhJkFwDB3CeBaHoEIYR%2BhlEGFwfI6RXFZGSeMHJhQGDTAGPEbJHQ8ndFGAU1oMT7BVJGL0dJMwsmTBqTkOpE4pgtLKRIBYChB6rH6foVue9zEH04OXfwKF3ELwWlY/cn8fIaBWbsCAuBCBYw2AxXg49zFzCnokLgc9V4AE4zmoUkP4a5XAEgJBbqMne4zcGH2Pqfc%2BBzSBX0QCAJYBAwwEEfvpL0L96DEAiDiTgqgZlzMkAsrWuxlmrKYfgP49s9A6OEKIcQhisUmLUPvXQ2SDTXGSMgpuYzSAd1eZwWB6tAWaSoNM2Z8z35LJQistZEAPB33BQFBIuzPlaEOaQae9y573KldK6VTzd7Uv3m82wHz9kispRwMwLyu6cD2RfBYYj0jOEkEAA%3D%3D%3D)
+        T delta_;
         T from_;
         T to_;
-        T delta_;
 
         typedef T value_type;
         typedef T size_type;
@@ -188,16 +192,16 @@ class range_t
         };
 
         constexpr range_t()
-        : from_(0), to_(0), delta_(1) {}
+        : delta_(1), from_(0), to_(0) {}
 
         constexpr range_t(T to)
-        : from_(0), to_(to), delta_(1) {}
+        : delta_(1), from_(0), to_(to) {}
 
         constexpr range_t(T from, T to)
-        : from_(from), to_(to), delta_(1) {}
+        : delta_(1), from_(from), to_(to) {}
 
         constexpr range_t(T from, T to, T delta) __attribute__((always_inline))
-        : from_(from), delta_(delta)
+        : delta_(delta), from_(from)
         {
             if (delta > 0)
                 to_ = from+((to-from+delta-1)/delta)*delta;
@@ -257,6 +261,16 @@ class range_t
             return to_-delta_;
         }
 
+        value_type from() const
+        {
+            return from_;
+        }
+
+        value_type to() const
+        {
+            return to_;
+        }
+
         value_type operator[](size_type n) const
         {
             return from_+n*delta_;
@@ -297,19 +311,121 @@ class range_t
             return shifted;
         }
 
+        /**
+         * Shift a range to the right.
+         *
+         * @param shift     The amount to shift, in units of the range step size.
+         *
+         * @param other     The range to shift.
+         *
+         * @return          The shifted range. After shifting, `other[i]` becomes
+         *                  `other[i] + shift*other.step()`.
+         *
+         * @ingroup range
+         */
         friend range_t operator+(T shift, const range_t& other)
         {
             return other + shift;
         }
 
+        /**
+         * Shift a range to the left.
+         *
+         * @param shift     The amount to shift, in units of the range step size.
+         *
+         * @param other     The range to shift.
+         *
+         * @return          The shifted range. After shifting, `other[i]` becomes
+         *                  `other[i] - shift*other.step()`.
+         *
+         * @ingroup range
+         */
         friend range_t operator-(T shift, const range_t& other)
         {
             return range_t(shift - other.from_, shift - other.to_, -other.delta_);
         }
 
+        /**
+         * Concatenate two adjacent ranges.
+         *
+         * @param other     The range to concatenate with `this`. The ranges must be
+         *                  adjacent, meaning that `this.to() == other.from()`, and
+         *                  both ranges must have the same step.
+         *
+         * @return          The combined range.
+         */
+        range_t operator|(const range_t& other) const
+        {
+            MARRAY_ASSERT(step() == other.step());
+            MARRAY_ASSERT(to() == other.from());
+            return range_t{from(), other.to(), step()};
+        }
+
+        /**
+         * Append an element to a range.
+         *
+         * @param lhs   The range to which to append.
+         *
+         * @param rhs   A range element. Must be equal to `lhs.to()`, that is, the element
+         *              immediately following the last element of `lhs` during iteration.
+         *
+         * @return      The combined range.
+         *
+         * @ingroup range
+         */
+        friend range_t operator|(const range_t& lhs, T rhs)
+        {
+            MARRAY_ASSERT(lhs.to() == rhs);
+            return range_t{lhs.from(), lhs.to()+lhs.step(), lhs.step()};
+        }
+
+        /**
+         * Prepend an element to a range.
+         *
+         * @param lhs   A range element. Must be equal to `rhs.from()-rhs.step()`, that is,
+         *              the element immediately preceding the first element of `rhs` during iteration.
+         *
+         * @param rhs   The range to which to prepend.
+         *
+         * @return      The combined range.
+         *
+         * @ingroup range
+         */
+        friend range_t operator|(T lhs, const range_t& rhs)
+        {
+            MARRAY_ASSERT(lhs+rhs.step() == rhs.from());
+            return range_t{lhs, rhs.to(), rhs.step()};
+        }
+
+        /**
+         * Create a range with reversed iteration order.
+         *
+         * @return  A range which iterates over the same set of elements as `this`,
+         *          but in reverse order.
+         */
         range_t reverse()
         {
             return range_t(back(), front()-step(), -step());
+        }
+
+        /**
+         * Test for emptiness.
+         *
+         * @return  `size() == 0`.
+         */
+        bool empty() const
+        {
+            return size() == 0;
+        }
+
+        /**
+         * Truth test for non-zero size.
+         *
+         * @return  `size() != 0`, or equivalently `!empty()`.
+         */
+        explicit operator bool() const
+        {
+            return size();
         }
 };
 
@@ -547,6 +663,6 @@ auto reversed_rangeN(T to, U N, V delta)
     return range(to-delta, to-(N+1)*delta, -delta);
 }
 
-}
+} // namespace MArray
 
 #endif //MARRAY_RANGE_HPP
